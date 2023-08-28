fec48989aeec57a87f1f59d2cd730e962cac1fa4
vikash0p <vikash2000158gmail.com>
2023-08-28 19:07:28 +0530

    change the file structure in the page js.

--- app/about/page.js
+++ app/about/page.js
@@ -4,6 +4,8 @@ const page = () => {
   return (
     <div>
       <h1>about flie</h1>
+      <h1>about file Lorem ipsum dolor sit amet.</h1>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, cum.</h3>
     </div>
   )
 }

86b45451029ddd1575387e17ad4cd01627faf98a
vikash0p <vikash2000158gmail.com>
2023-08-28 16:36:20 +0530

    add a paragraph to page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ const page = () => {
   return (
     <div>
       <h1>vikashpant</h1>
+      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex nesciunt numquam, nam non voluptatum cumque magnam veniam dolor distinctio alias!</h1>
     </div>
   )
 }
